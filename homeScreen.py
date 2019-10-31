8bb29d6b0c7313dbcbde7649b790258d268dadfe
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2019-10-31 19:01:29 +0300

    v 0.1.2
    homescreen güncellemesi yapıldı.

--- homeScreen.py
+++ homeScreen.py
@@ -1,3 +1,24 @@
+from tkinter import * 
+import tkinter as tk
+from tkinter import ttk
+from tkinter import filedialog
+
 class HomeScreen:
     def __init__(self):
         self.frameSize=()
+
+    def createHomeScreen(self):     
+        root = Tk('Main')      
+        root.title("Captcha With Stegonography")
+        img=PhotoImage(file="images/shoes.jpg")
+        lblIm=Label(root,image=img)
+        lblIm.image=img
+        lblIm.pack()
+        lblIm.place(x=10,y=10,width=120,height=50)
+        mainloop()  
+
+hm=HomeScreen()
+hm.createHomeScreen()
+
+
+
