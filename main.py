820ceec609ed6ade06c60e7ddcd59d463ff7fe40
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2019-11-04 15:58:16 +0300

    v 0.1.6
    yeni modüller eklendi.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from imageParser import ParseProcess as pp
 from stegonography import StegonographyProcess as sp
 from homeScreen import HomeScreen as hm
 
-pp=pp(9,0,"images/shoes.jpg")
+pp=pp(16,0,"images/shoes.jpg")
 
 frames=pp.getFrames(0)
 
