8224eb983189ec5902401ce1fe9039bb0adb19cc
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2019-11-09 14:56:51 +0300

    v 0.3.2
    userLogin.py dosyası güncellendi.

--- userLoginScreen.py
+++ userLoginScreen.py
@@ -1,3 +1,4 @@
+#database bağlantılı olan kod
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
@@ -94,3 +95,51 @@ if __name__ == "__main__":
     root = tk.Tk()
     loginForm(root)
     root.mainloop()
+
+
+
+#Güncel çalışan kod 
+####################################################################################
+from tkinter import *
+import tkinter.messagebox as tm
+
+
+class LoginFrame(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_username = Label(self, text="Username")
+        self.label_password = Label(self, text="Password")
+
+        self.entry_username = Entry(self)
+        self.entry_password = Entry(self, show="*")
+
+        self.label_username.grid(row=0, sticky=E)
+        self.label_password.grid(row=1, sticky=E)
+        self.entry_username.grid(row=0, column=1)
+        self.entry_password.grid(row=1, column=1)
+
+        self.checkbox = Checkbutton(self, text="Keep me logged in")
+        self.checkbox.grid(columnspan=2)
+
+        self.logbtn = Button(self, text="Login", command=self._login_btn_clicked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def _login_btn_clicked(self):
+        # print("Clicked")
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+
+        # print(username, password)
+
+        if username == "john" and password == "password":
+            tm.showinfo("Login info", "Welcome John")
+        else:
+            tm.showerror("Login error", "Incorrect username")
+
+
+root = Tk()
+lf = LoginFrame(root)
+root.mainloop()
