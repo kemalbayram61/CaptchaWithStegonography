7c6ef560750f52894be9384187cf3f0ddd492c8a
Kemal Bayram <36696235+kemalbayram61@users.noreply.github.com>
2019-12-04 00:44:46 +0300

    v 0.3.6
    databaseOperation modülü bütün nesnelerin veri tabanı ilişkileri için güncellendi.

--- databaseOperation.py
+++ databaseOperation.py
@@ -1,59 +1,141 @@
-import mysql.connector
+import sqlite3
 
-class DatabaseManip():
-    def __init__(self,username,password):
-        self.username = username
-        self.password = password
-        #
-        # print(self.username)
-        # print(self.password)
+class dbOperations:
+    database=''
+    databaseName=''
+    im=''
+    def __init__(self,databaseName):
+        self.databaseName=databaseName
+        self.initializeConnection()
+        
+    def initializeConnection(self):
+        self.database=sqlite3.connect(self.databaseName)
 
-        self.dbaseInitializer()
+    def executeQuery(self,query):
+        self.im=self.database.cursor()
+        self.im.execute(query)
+        self.database.commit()
+        return self.im
 
-    def dbaseInitializer(self):
-        mydb = mysql.connector.connect(
-            host="127.0.0.1",
-            user="root",
-            password="admin123",
-            database="testdb"
-        )
+class UserOperations:
+    databaseName=''
+    database=''
+    query=''
+    def __init__(self,databaseName):
+        self.databaseName=databaseName
+        self.database=dbOperations(self.databaseName)
 
-        self.myCursor = mydb.cursor()
-        query_user_exists = "SELECT EXISTS(SELECT * FROM users WHERE username = %s)"
-        self.myCursor.execute(query_user_exists,(self.username,))
-        tuple_user_exists = self.myCursor.fetchone()
-        total_user_exists = int(tuple_user_exists[0])
+    def addUser(self,tcNumber,firstName,lastName,birthday,password,address):
+        self.query="insert into users (tcNumber,firstName,lastName,password,birthday,address) values (\""+tcNumber+"\",\""+firstName+"\",\""+lastName+"\",\""+password+"\",\""+birthday+"\",\""+address+"\")"
+        self.database.executeQuery(self.query)
+        return True
 
-        if total_user_exists != 0:
-            query_pass_validation = "SELECT password FROM users WHERE username = %s"
-            self.myCursor.execute(query_pass_validation,(self.username,))
-            __tuple_valid_pass = self.myCursor.fetchone()
-            __password_validation = __tuple_valid_pass[0]
+    def updateUser(self,tcNumber,firstName,lastName,birthday,password,address):
+        self.query="update users set firstName=\""+firstName+"\",lastName=\""+lastName+"\",tcNumber=\""+tcNumber+"\",birthday=\""+birthday+"\",password=\""+password+"\",address=\""+address+"\" where tcNumber=\""+tcNumber+"\""
+        self.database.executeQuery(self.query)
+        return True
 
-            if __password_validation == self.password:
+    def dropUser(self,tcNumber):
+        self.query="delete from users where tcNumber=\""+tcNumber+"\""
+        self.database.executeQuery(self.query)
+        return True
 
-                self.verdict = "correct"
-            else:
-                self.verdict = "incorrect"
+    def getUser(self,tcNumber):
+        self.query="select * from users where tcNumber=\""+tcNumber+"\""
+        return self.database.executeQuery(self.query).fetchall()
 
-        else:
-            self.verdict = "notexist"
+    def getUsers(self):
+        self.query="select * from users"
+        return self.database.executeQuery(self.query).fetchall()
 
-    def __str__(self):
-        return self.verdict
+class imageOperations:
+    databaseName=''
+    database=''
+    query=''
+    def __init__(self,databaseName):
+        self.databaseName=databaseName
+        self.database=dbOperations(self.databaseName)
+
+    def addImage(self,imageID,filePath,imageName,objectFrames):
+        self.query="insert into images values("+(str)(imageID)+",\""+filePath+"\",\""+imageName+"\",\""+objectFrames+"\")"
+        self.database.executeQuery(self.query)
+        return True
+
+    def dropImage(self,imageID):
+        self.query="delete from images where imageID="+(str)(imageID)+""
+        self.database.executeQuery(self.query)
+        return True
+
+    def updateImage(self,lastImageId,filePath):
+        self.query="update images set imageID="+(str)(lastImageId)+",filePath=\""+filePath+"\" where imageID="+(str)(lastImageId)+""
+        self.database.executeQuery(self.query)
+        return True
+
+    def getImage(self,imageID):
+        self.query="select * from images where imageID="+(str)(imageID)+""
+        return self.database.executeQuery(self.query).fetchall()
+
+    def getImages(self):
+        self.query="select * from images"
+        return self.database.executeQuery(self.query).fetchall()
+
+class QuestionOperations:
+    databaseName=''
+    database=''
+    query=''
+    def __init__(self,databaseName):
+        self.databaseName=databaseName
+        self.database=dbOperations(self.databaseName)
+
+    def addQuestion(self,questionID,question):
+        self.query="insert into questions values ("+(str)(questionID)+",\""+question+"\")"
+        self.database.executeQuery(self.query)
+        return True
+
+    def dropQuestion(self,questionID):
+        self.query="delete from questions where questionID="+(str)(questionID)+""
+        self.database.executeQuery(self.query)
+        return True
+
+    def getQuestion(self,questionID):
+        self.query="select * from questions where questionID="+(str)(questionID)+""
+        return self.database.executeQuery(self.query).fetchall()
+
+    def updateQuestions(self,questionID,question):
+        self.query="update questions set questionID="+(str)(questionID)+",question=\""+question+"\" where questionID="+(question)(questionID)+""
+        self.database.executeQuery(self.query)
+        return True
+
+    def getQuestions(self):
+        self.query="select * from questions"
+        return self.database.executeQuery(self.query).fetchall()
+
+class AnswerOperations:
+    databaseName=''
+    database=''
+    query=''
+    def __init__(self,databaseName):
+        self.databaseName=databaseName
+        self.database=dbOperations(self.databaseName)
+
+    def addAnswer(self,answerID,questionID,userTc,answer):
+        self.query="insert into answers values("+(str)(answerID)+","+(str)(questionID)+",\""+userTc+"\",answer=\""+answer+"\")"
+        self.database.executeQuery(self.query)
+        return True
+
+    def dropAnswer(self,answerID,userTc):
+        self.query="delete from answers where userTc=\""+userTc+"\" and answerID="+(str)(answerID)+""
+        self.database.executeQuery(self.query)
+        return True
+
+    def updateAnswer(self,userTc,answerID,answer):
+        self.query="update answers set answer=\""+answer+"\"  where userTc=\""+userTc+"\" and answerID="+(str)(answerID)+""
+        self.database.executeQuery(self.query)
+        return True
+
+    def getAnswers(self,userTc):
+        self.query="select * from answers where userTc="+(str)(userTc)+""
+        return self.database.executeQuery(self.query).fetchall()
 
 
-    @property
-    def username(self):
-        return self.__username
-    @username.setter
-    def username(self,username):
-        self.__username = username
 
-
-    @property
-    def password(self):
-        return self.__password
-    @password.setter
-    def password(self,password):
-        self.__password = password
